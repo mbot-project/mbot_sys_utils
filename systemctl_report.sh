867c408923657aea14fe42a635e14318fbabf172
Jana Pavlasek <jbpavlasek@gmail.com>
2024-09-05 12:05:09 -0400

    Delete deprecated script

--- systemctl_report.sh
+++
@@ -1,7 +0,0 @@
-services=("mbot-start-network.service" "mbot-publish-info.service" "mbot-rplidar-driver.service" "mbot-lcm-serial.service" "mbot-web-server.service" "mbot-motion-controller.service" "mbot-slam.service" "mbot-oled.service")
-
-# Grab the third line
-for service in "${services[@]}"; do
-    echo "$service"
-    systemctl status $service | head -3 | tail -1
-done
