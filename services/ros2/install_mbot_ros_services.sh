e6eb1e73bd4ec666215de45b21f83a4614b4019b
Shaw <sunxycn98@gmail.com>
2025-06-17 20:43:40 -0400

    start the system service as well

--- services/ros2/install_mbot_ros_services.sh
+++ services/ros2/install_mbot_ros_services.sh
@@ -15,7 +15,7 @@ sudo systemctl enable --now systemd-time-wait-sync.service
 # Enable the services.
 sudo systemctl daemon-reload
 sudo systemctl enable mbot-oled.service
-
+sudo systemctl start mbot-oled.service
 # Success message.
 echo
-echo "Installed and enabled the OLED display service."
+echo "Installed, enabled, and started the OLED display service."
